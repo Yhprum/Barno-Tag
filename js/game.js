c7a50a45aa99cd339dda128c2e5190a4f6be42e4
Yhprum <ryanmurphy@gatech.edu>
2018-01-10 16:40:45 -0500

    Fixed movement

--- js/game.js
+++ js/game.js
@@ -28,43 +28,72 @@ $(document).ready(function() {
 
 
     //user variables
+    var up = 0, down = 0, left = 0, right = 0;
     var x = 1;
     var y = 1;
+    var oldx = 1
+    var oldy = 1;
     var i = 0;
     var moving = false;
     var direction = [0, 0];
-    var movingDirection = [0, 0];
 
     document.onkeydown = function(e) {
         e = e || window.event;
-        if (e.keyCode == '38' && map[y-1][x] == 0) { // up
-            y--;
-            direction = [0, -1];
-        } else if (e.keyCode == '40' && map[y+1][x] == 0) { // down
-            y++;
-            direction = [0, 1];
-        } else if (e.keyCode == '37' && map[y][x-1] == 0) { // left
-           x--;
-           direction = [-1, 0];
-        } else if (e.keyCode == '39' && map[y][x+1] == 0) { // right
-           x++;
-           direction = [1, 0];
+        if (e.keyCode == '38') {
+            up = 1
+            down = 0
+            left = 0
+            right = 0
+        } else if (e.keyCode == '40') {
+            up = 0
+            down = 1
+            left = 0
+            right = 0
+        } else if (e.keyCode == '37') {
+            up = 0
+            down = 0
+            left = 1
+            right = 0
+        } else if (e.keyCode == '39') {
+            up = 0
+            down = 0
+            left = 0
+            right = 1
         }
     }
-    function move(direction) {
+    document.onkeyup = function(e) {
+        e = e || window.event;
+        if (e.keyCode == '38') up = 0
+        else if (e.keyCode == '40') down = 0
+        else if (e.keyCode == '37') left = 0
+        else if (e.keyCode == '39') right = 0
+    }
+    function move(mdirection) {
         context.drawImage(maze, 0, 0, 1024, 704);
-        context.fillRect(x*64 + i*direction[0], y*64 + i*direction[1], 64, 64)
-        i++;
+        context.fillRect(oldx*64 + i*mdirection[0], oldy*64 + i*mdirection[1], 64, 64)
+        if (i++ >= 64) {
+            updatePosition()
+        }
     }
     window.setInterval(update, 5); // change tp global speed for easy changing?
 
     function update() {
-        if (!moving && (direction[0] || direction[1])) {
+        if (!moving && (up||down||left||right)) {
+            if (map[y + down - up][x + right - left] == 0) {
+                direction = [right-left, down-up]
+                x += direction[0];
+                y += direction[1];
+                move(direction);
+                moving = true;
+            }
+        } else if (moving) {
             move(direction);
-            moving = true;
-            movingDirection = direction;
-        } else if (movingDirection[0] || movingDirection[1]) {
-            move(movingDirection);
         }
+    }
+    function updatePosition() {
+        i = 0;
+        moving = false;
+        oldx = x;
+        oldy = y;
     }
 });
